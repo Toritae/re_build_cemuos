076e654e2d17ad7f1a3419f9e38b4cd248d4ef0f
Toritae <ilieys6482@gmail.com>
2021-10-08 01:29:10 +0900

    debug

--- pj_board_gov/urls.py
+++ pj_board_gov/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('summernote/', include('django_summernote.urls')),
     path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/delete', views.delete, name='delete'),
-    path('<int:pk>/edit', views.notice_edit_view, name='update'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.notice_edit_view, name='update'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
