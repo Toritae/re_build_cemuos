5db4736528e494ad8fceab27b5277974bc4629b1
Toritae <ilieys6482@gmail.com>
2021-10-10 01:23:55 +0900

    debug

--- reference_board/models.py
+++ reference_board/models.py
@@ -6,8 +6,8 @@ class DataRoom(models.Model):
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
     title = models.CharField(max_length=128, verbose_name='제목',blank=True)
     content = models.TextField(verbose_name='내용',blank=True)
-    create_date = models.DateTimeField(default=timezone.now())
-    upload_files = models.FileField(upload_to='media/reference_room/',blank=True)
+    create_date = models.DateTimeField(default=timezone.now)
+    upload_files = models.FileField(upload_to='media/reference_room/',null=True,blank=True)
     
     class Meta:
         db_table = 'DataRoom'
