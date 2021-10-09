330cdcb487f54bb8ab4366dbbc85e93717b2ac76
Toritae <ilieys6482@gmail.com>
2021-10-10 01:08:41 +0900

    debug

--- reference_board/models.py
+++ reference_board/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 class DataRoom(models.Model):
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
     title = models.CharField(max_length=128, verbose_name='제목',blank=True)
     content = models.TextField(verbose_name='내용',blank=True)
-    create_date = models.DateTimeField(auto_now_add=True)
-    upload_file = models.FileField(upload_to='media/reference_room/',blank=True)
+    create_date = models.DateTimeField(default=timezone.now())
+    upload_files = models.FileField(upload_to='media/reference_room/',blank=True)
     
     class Meta:
         db_table = 'DataRoom'
