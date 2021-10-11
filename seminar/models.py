17f715bf79e342fb32f901ff7cb003d3e60b29d9
Toritae <ilieys6482@gmail.com>
2021-10-12 05:34:00 +0900

    .

--- seminar/models.py
+++ seminar/models.py
@@ -3,10 +3,10 @@ from django.conf import settings
 from django.utils import timezone
 # Create your models here.
 class seminar_post(models.Model):
-    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='작성자')
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True,null=True, verbose_name='작성자')
     title = models.CharField(max_length=128, verbose_name='제목',blank=True)
     content = models.TextField(verbose_name='내용',blank=True)
-    create_date = models.DateTimeField(default=timezone.now)
+    create_date = models.DateTimeField(default=timezone.now,blank=True)
     upload_files = models.FileField(upload_to='media/seminar/',null=True,blank=True)
     filename = models.CharField(max_length=64, null=True, verbose_name='첨부파일명',blank=True)
     
